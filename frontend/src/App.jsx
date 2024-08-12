6dade314c121c5807e80e621a52bad15d1880586
Med Ali <111361086+TCHOOP22@users.noreply.github.com>
2024-08-12 20:47:17 +0100

    config tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import TodoList from "./pages/TodoList";
-// import "./ind";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
       <TodoList />
     </>
   );
