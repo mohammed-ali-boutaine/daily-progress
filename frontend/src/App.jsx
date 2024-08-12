b6c2b3c28ce47d49cddfa615eb958cd2e0695adc
Med Ali <111361086+TCHOOP22@users.noreply.github.com>
2024-08-12 20:44:18 +0100

    install tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import TodoList from "./pages/TodoList"
-
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import TodoList from "./pages/TodoList";
+// import "./ind";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-
   return (
     <>
-    <TodoList/>
+      <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
+      <TodoList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
